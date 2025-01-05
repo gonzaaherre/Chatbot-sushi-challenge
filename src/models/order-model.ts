8c5a320df8e93108f123ce1abe88a28cd26d5c7a
gonzaaherre <gonzaaherre@gmail.com>
2025-01-05 15:36:45 -0300

    agrego status en order

--- src/models/order-model.ts
+++ src/models/order-model.ts
@@ -6,23 +6,32 @@ export interface Order extends Document {
     quantity: number;
   }>;
   totalPrice: number;
+  status: "pending" | "completed" | "cancelled";
   createdAt: Date;
+  updatedAt: Date;
 }
 
-const orderSchema = new Schema({
-  products: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
+const orderSchema = new Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: { type: Number, required: true },
       },
-      quantity: { type: Number, required: true },
+    ],
+    totalPrice: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "completed", "cancelled"],
+      default: "pending",
     },
-  ],
-  totalPrice: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+  },
+  { timestamps: true } // createdAt y updatedAt automáticos
+);
 
 const Order = mongoose.model<Order>("Order", orderSchema);
 
